7c75a869e30d4a915a5dcae3b0a004bf1011374c
Freya <752642+skyelights@users.noreply.github.com>
2022-02-06 18:50:48 +0100

    release 1.0.10
    fix: discord token

--- src/app/app.ts
+++ src/app/app.ts
@@ -20,7 +20,7 @@ export const APP = {
         windowWidth: 1280,
         windowHeight: 720,
         deezerUrl: 'https://www.deezer.com/login',
-        discordClientID: ''
+        discordClientID: '%DISCORD_TOKEN%'
     },
     preferences: {
         closeToTray: 'closeToTray',
